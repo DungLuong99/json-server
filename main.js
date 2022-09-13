15dbb5f10840976755d4ada6832a62cf41f5d624
DungLuong99 <dunglt2301@gmail.com>
2022-09-13 17:29:58 +0700

    change port

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3000;
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
@@ -25,6 +26,6 @@ server.use((req, res, next) => {
 
 // Use default router
 server.use(router)
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
